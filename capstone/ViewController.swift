bd853c7426002e3bdf5c7e09b031c60564015390
Vanessa Tang <vanesst5@uci.edu>
2023-11-11 11:07:43 -0800

    Removed some outlets to reconfigure table view correctly

--- capstone/ViewController.swift
+++ capstone/ViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
+
+    @IBOutlet weak var topRatedTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        topRatedTableView.dataSource=self
+        
     }
 
 
